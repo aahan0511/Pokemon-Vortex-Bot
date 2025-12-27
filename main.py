845f6719999d18f483afd7af1b0f3c6277af1182
Aahan Salecha <188063520+aahan0511@users.noreply.github.com>
2025-12-27 20:43:35 +0530

    Remove Login functionality

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ from pandas import DataFrame
 
 dotenv.load_dotenv()
 SESSION_ID = os.getenv("SESSION_ID")
-USERNAME = os.getenv("USER")
-PASSWORD = os.getenv("PWD")
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -22,50 +20,7 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger()
-logger.debug("Application started")
-
-
-def login():
-    url = "https://www.pokemon-vortex.com/login-auth/"
-
-    payload = {
-        "myusername": USERNAME,
-        "mypassword": PASSWORD,
-        "tz": "Asia/Calcutta",
-        "submit": "Log In",
-    }
-    headers = {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-        "accept-language": "en-US,en;q=0.9",
-        "cache-control": "max-age=0",
-        "content-type": "application/x-www-form-urlencoded",
-        "dnt": "1",
-        "origin": "https://www.pokemon-vortex.com",
-        "priority": "u=0, i",
-        "referer": "https://www.pokemon-vortex.com/login/?action=logout",
-        "sec-ch-ua": '"Brave";v="143", "Chromium";v="143", "Not A(Brand";v="24"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"Windows"',
-        "sec-fetch-dest": "document",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "same-origin",
-        "sec-fetch-user": "?1",
-        "sec-gpc": "1",
-        "upgrade-insecure-requests": "1",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
-        "Cookie": f"SESS={SESSION_ID}",
-    }
-
-    response = requests.request("POST", url, headers=headers, data=payload)
-
-    if "The username or password you entered is incorrect." in response.text:
-        logger.critical("LOGIN FAILED: Invalid Credentials")
-        raise Exception("Invalid credentials")
-
-    if USERNAME in response.text:
-        logger.info("LOGIN SUCCESSFUL")
-    else:
-        logger.warning("LOGIN UNSURE: Username not in Response")
+logger.debug("APPLICATION STARTED")
 
 
 def browse_auctions(filter_type):
@@ -132,6 +87,37 @@ def browse_auctions(filter_type):
     return df
 
 
+def check_dashboard():
+    url = "https://www.pokemon-vortex.com/dashboard/"
+
+    payload = {}
+    headers = {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "max-age=0",
+        "dnt": "1",
+        "priority": "u=0, i",
+        "referer": "https://www.pokemon-vortex.com/login/?action=logout",
+        "sec-ch-ua": '"Brave";v="143", "Chromium";v="143", "Not A(Brand";v="24"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-user": "?1",
+        "sec-gpc": "1",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
+        "Cookie": f"SESS={SESSION_ID}",
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    with open("dashboard.html", "w") as f:
+        f.write(response.text)
+
+
 if __name__ == "__main__":
     # login()
-    browse_auctions("items")
+    check_dashboard()
+    # browse_auctions("items")
+    logger.debug("APPLICATION ENDED")
